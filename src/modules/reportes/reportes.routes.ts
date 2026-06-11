5a467eaa142300de756b99794643867bfcf6f18f
JuanAntonioSantiago015 <juanantoniosantiago92@gmail.com>
2026-06-11 12:13:37 -0600

    feat: rutas generales

--- src/modules/reportes/reportes.routes.ts
+++ src/modules/reportes/reportes.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { requireAuth } from "../../common/middleware/auth.middleware";
+import { reporteOrdenesHandler } from "./ordenes/ordenes-report.controller";
 import { reportePagosHandler } from "./reportes.controller";
 
 const router = Router();
@@ -7,5 +8,6 @@ const router = Router();
 router.use(requireAuth);
 
 router.get("/pagos", reportePagosHandler);
+router.get("/ordenes", reporteOrdenesHandler);
 
 export default router;
