e59de543a028c197cf27a4d516b09afa1eefbea5
JuanAntonioSantiago015 <juanantoniosantiago92@gmail.com>
2026-06-23 10:00:21 -0600

    fix: pequeña corrección a routes

--- src/modules/reportes/reportes.routes.ts
+++ src/modules/reportes/reportes.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { requireAuth } from "../../common/middleware/auth.middleware";
 import ordenesReportRoutes from "./ordenes/ordenes-report.routes";
-// import pagosReportRoutes from "./pagos/pagos-report.routes";
+import pagosReportRoutes from "./pagos/pagos-report.routes";
 
 const router = Router();
 
 router.use(requireAuth);
 
+router.use("/pagos", pagosReportRoutes);
 router.use("/ordenes", ordenesReportRoutes);
-// router.use("/pagos", pagosReportRoutes);
 
 export default router;
