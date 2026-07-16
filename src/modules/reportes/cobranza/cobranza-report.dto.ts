a806d50b4b7d7be2a9c6512a8c488454c43b2834
JuanAntonioSantiago015 <juanantoniosantiago92@gmail.com>
2026-07-16 12:21:41 -0600

    feat: dto

---
+++ src/modules/reportes/cobranza/cobranza-report.dto.ts
@@ -0,0 +1,42 @@
+import { EstadoCargo, EstadoCuentaServicio } from "@prisma/client";
+
+export interface CobranzaReportFilters {
+    codigoCliente?: string;
+    nombreCliente?: string;
+    estadoCargo?: EstadoCargo[];
+    estadoServicio?: EstadoCuentaServicio;
+    tipoServicioId?: string;
+    zona?: number;
+}
+
+export interface CobranzaReportKpis {
+    totalPendiente: number;
+    cuentasPendientes: number;
+}
+
+export interface CobranzaReportRow {
+    clienteId: string;
+    clienteNombre: string;
+    cuentaServicioId: string;
+    cuentaServicioNombre: string;
+    tipoServicio: string;
+    periodo: string;
+    concepto: string;
+    fechaVencimiento: string;
+    saldoPendiente: number;
+    estadoCargo: EstadoCargo;
+    estadoServicio: EstadoCuentaServicio;
+}
+
+export interface PaginationDto {
+    page: number;
+    pageSize: number;
+    total: number;
+    totalPages: number;
+}
+
+export interface CobranzaReportResponse {
+    kpis: CobranzaReportKpis;
+    rows: CobranzaReportRow[];
+    pagination: PaginationDto;
+}
